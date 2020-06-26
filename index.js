9640ae05f7cbc250e1e86654901645e9edef87c5
BenWix <benwichser@gmail.com>
2020-06-26 14:34:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail
+    };
+    console.log(JSON.stringify(formData))
+
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+       
+
+    fetch("http://localhost:3000/users",configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            appendOutput(object)
+        })
+        .catch(function(error) {
+            alert("There was an error!!")
+            console.log(error)
+            appendOutput(error.message)
+        })
+
+}
+
+function appendOutput(output) {
+    let domObject = document.querySelector("body")
+    let newElement = document.createElement("p")
+    newElement.innerText = output
+    domObject.appendChild(newElement)
+}
