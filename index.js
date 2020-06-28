446227581f9cbb385e96589e7563a069410845d9
ferrisbueller66 <github email address>
2020-06-28 19:56:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+function submitData (username, email) {
+    let formData = {
+        name: username,
+        email: email
+      };
+       
+
+let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      let newUser = document.createElement('p');
+        newUser.id = object.id;
+      let newId = document.createTextNode(`${object.id}`);
+        newUser.appendChild(newId);
+        document.body.appendChild(newUser);    
+    })
+    .catch(function(error) {
+      
+      let newMessage = document.createElement('p');
+      newMessage.id = "Unauthorized Access";
+    let newNode = document.createTextNode(`${newMessage.id}`);
+      newMessage.appendChild(newNode);
+      document.body.appendChild(newMessage);  
+
+
+      alert("Unauthorized Access");
+      console.log(error.message);
+  
+    });
+
+};
