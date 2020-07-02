8f074d1bfc421c43886d97aa9174cd4e6b1eb9f1
rwparrish <rino3717@gmail.com>
2020-07-02 11:04:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+function submitData() {
+    
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+      };
+       
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(object) {
+        let h3 = document.createElement('h3')
+        h3.innerHTML = object.id
+        document.body.appendChild(h3)
+    })
+
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        let h3 = document.createElement('h3')
+        h3.innerHTML = error.message
+        document.body.appendChild(h3)
+      });
+
+
+
+    
+}
