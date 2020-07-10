8a727552adbd750f919bc67183ffb57857dfa41e
nisticism <nistacular@gmail.com>
2020-07-09 21:27:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData() {
+    return fetch("http://localhost:3000/users", configObj).
+        then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        document.querySelector('body').innerHTML += object.id;
+      })
+      .catch(function(error) {
+        alert("Unauthorized Access");
+        document.querySelector('body').textContent += error.message;
+      });;
+
+}
+
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  fetch("http://localhost:3000/dogs", configObj);
+
